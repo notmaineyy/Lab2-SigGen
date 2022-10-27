68c6a6892082632e96389579f0b5558e916ce6ba
notmaineyy <shermaineang2802@gmail.com>
2022-10-27 11:06:07 +0100

    completed task 2

--- notes.md
+++ notes.md
@@ -9,3 +9,8 @@
 Python file generates sine wave outputs and creates the file **_sinegen.mem_** 
 
 **_counter.sv_** has a new input "incr"
+
+### Task 2
+Added a new input "off" to represent offset, that is controlled by Vbdvalue().
+This is the input to rom.sv and there is an additional output "dout2". 
+ROM now has an additional input and 2 outputs (dout and dout2), where dout2 is the original wave with the offset.
---
+++ task2/rom.sv
@@ -0,0 +1,26 @@
+module rom #(
+    parameter   ADDRESS_WIDTH = 8,
+                DATA_WIDTH = 8
+)(
+    input logic                      clk,
+    input logic [ADDRESS_WIDTH-1:0] addr,
+    input logic [DATA_WIDTH-1:0]    off,
+    output logic [DATA_WIDTH-1:0]   dout,
+    output logic [DATA_WIDTH-1:0]   dout2
+);
+
+logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+
+initial begin
+    $display("Loading rom.");
+    $readmemh("sinerom.mem", rom_array);
+end;
+
+always_ff @(posedge clk)
+    begin
+    // output is synchronous
+    dout <= rom_array [addr];
+    dout2 <= rom_array [addr+off];
+    end
+
+endmodule
